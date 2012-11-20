9701ead6e9cd4a8c7b87c7646f2c9d5996257cd9
Sam Machin <sam@sammachin.com>
2012-11-20 09:17:03 +0000

    1st Commit

---
+++ webswitch.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+#
+# ServerSide Component of a browser based switch 
+#
+# Sam Machin - theLab Nov 2012
+#
+
+import cherrypy
+import urllib
+import urllib2
+import json
+import memcache
+import os
+
+
+mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+STATIC_DIR = os.path.join(os.path.abspath("."), u"static")
+
+
+
+class start(object):
+	def index(self):
+		return open(os.path.join(STATIC_DIR, u'index.html'))
+	def state(self, var=None, **params):
+		switch = str(urllib.unquote(cherrypy.request.params['switch']))
+		state = mc.get(switch)
+		data = {}
+		data['switch'] = []
+		data['switch'].append(state)
+		return json.dumps(data)
+	def on(self, var=None, **params):
+		switch = str(urllib.unquote(cherrypy.request.params['switch']))
+		mc.set(switch, "on")
+		return "ok"
+	def off(self, var=None, **params):	
+		switch = str(urllib.unquote(cherrypy.request.params['switch']))
+		mc.set(switch, "off")
+		return "ok"
+	state.exposed = True
+	on.exposed = True
+	off.exposed = True
+
+cherrypy.config.update('/server/tucore/app.cfg')
+app = cherrypy.tree.mount(start(), '/', '/server/webswitch/app.cfg')
+cherrypy.config.update({'server.socket_host': '0.0.0.0',
+                        'server.socket_port': 9010})
+
+if hasattr(cherrypy.engine, "signal_handler"):
+    cherrypy.engine.signal_handler.subscribe()
+if hasattr(cherrypy.engine, "console_control_handler"):
+    cherrypy.engine.console_control_handler.subscribe()
+cherrypy.engine.start()
+cherrypy.engine.block()
